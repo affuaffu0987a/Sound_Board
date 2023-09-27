82e507feef689bbdd6ac6a176d9be2f59cd8a0cb
aftabkhan1234 <aftabkhan@gmail.com>
2023-09-27 10:35:39 +0530

    Sound_Board

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-const Allbtn = document.querySelectorAll(".sound-btn")
 
-let audio = new Audio("sound/tadaa.mp3")
+const sounds=[
+    {
+        id:0,
+        SoundURL:"sound/gasp.mp3"
+    },
+    {
+        id:1,
+        SoundURL:"sound/tadaa.mp3"
+    },
+    {
+        id:2,
+        SoundURL:"sound/victory.mp3"
+    },
+    {
+        id:3,
+        SoundURL:"sound/wrong-answer.mp3"
+    },
+    {
+        id:4,
+        SoundURL:"sound/crowdcheer.mp3"
+    },{
+        id:5,
+        SoundURL:"sound/boo.mp3"
+    }
+]
+
+const Allbtn = document.querySelectorAll(".sound-btn");
+
+
 Allbtn.forEach((e)=>{
-console.log(e);
-e.addEventListener("click",()=>{
-    audio.play()
+e.addEventListener("click",(elm)=>{
+    sounds.forEach((sound)=>{
+       if(elm.target.id == sound.id){
+        let audio = new Audio(sound.SoundURL)
+        audio.play()
+       }
+    })
 })
 })
 
